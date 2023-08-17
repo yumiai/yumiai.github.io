ea77345b71ca9a1790b6174c7798aec9bae3a982
Robert O’Brien <82072426+wolstaeb@users.noreply.github.com>
2023-08-18 11:41:12 +1200

    CHORE: Replicated existing style to njk base tmpl.

--- _config.ts
+++ _config.ts
@@ -6,32 +6,127 @@ import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
+import jsx from "lume/plugins/jsx_preact.ts";
 import pageFind from "lume/plugins/pagefind.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import feed from "lume/plugins/feed.ts";
 
-const site = lume({
-  location: new URL("https://example.com/"),
-});
+
+const site = lume({location: new URL("https://yumi.ai.com/")});
+
+// site.filter("random-delay", function(value) {return '${value}${Math(floor(Math.random()*5)}s';});
 
 site
   .ignore("README.md", "flake.nix", "flake.lock", "default.nix", "shell.nix")
   .copy("assets")
+  .copy("scripts")
+  .use(jsx())
   .use(tailwindcss({
     // Extract the classes from HTML and njk files
-    extensions: [".html", ".njk", ".css"],
+    extensions: [".html", ".njk", ".tsx", ".js", ".css"],
 
     // Tailwind options, like the theme colors and fonts
     options: {
+
+      plugins: [
+        function ({ addUtilities }) {
+          addUtilities({
+            '.mask-radial': {
+              maskImage: 'radial-gradient(67% 50% at center, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 80%)',
+              WebkitMaskImage: 'radial-gradient(67% 50% at center, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 80%)'
+            }
+          }, ['responsive', 'hover']);
+        },
+      ],
+
       theme: {
         extend: {
-          color: {
-            'yumi-green': '#012E0D',
+          colors: {
+            'coconut-cream': {
+              'DEFAULT': '#fcfaed',
+              '50': '#fcfaed',
+              '100': '#f6f1cf',
+              '200': '#ede29a',
+              '300': '#e3ce66',
+              '400': '#dcbb43',
+              '500': '#d4a02c',
+              '600': '#bb7f24',
+              '700': '#9c5e21',
+              '800': '#7f4b21',
+              '900': '#693e1e',
+              '950': '#3c200c',
+            },          
+            'english-holly': {
+              '50': '#effef2',
+              '100': '#d9ffe0',
+              '200': '#b5fdc3',
+              '300': '#7cf997',
+              '400': '#3cec62',
+              '500': '#12d53d',
+              '600': '#09b02d',
+              '700': '#0b8a27',
+              '800': '#0e6d24',
+              '900': '#0e5921',
+              'DEFAULT': '#012e0d',
+              '950': '#012e0d',
+            },             
+            'tuscany': {
+              '50': '#fcf7f0',
+              '100': '#f9ecdb',
+              '200': '#f1d7b7',
+              '300': '#e8bc89',
+              '400': '#de9759',
+              '500': '#d67c39',
+              'DEFAULT': '#cb672f',
+              '600': '#cb672f',
+              '700': '#a64f28',
+              '800': '#854027',
+              '900': '#6c3622',
+              '950': '#3a1a10',
+            },     
+            
+            // TODO: These colors are related to the mask-radical class above.
+            'black-alpha': 'rgba(0,0,0,1)',
+            'transparent': 'rgba(0,0,0,0)',
           },
-          fontFamily: {
-            'fraunces': ['Fraunces', 'Merriweather', 'serif' ],
-            'inter': ['Inter', 'Graphik', 'sans-serif' ],            
+          maxHeight: {
+            'viewport-200': 'calc(100vh - 200px)' // the class .max-h-viewport-200
           },
+          fontFamily: {      
+            'serif': ["Fraunces, Merriweather, serif", {fontVariationSettings: '"wght" 400, "SOFT" 100, "WONK" 1'} ],  
+            'header': ["Fraunces, Merriweather, serif", {fontVariationSettings: '"wght" 400, "SOFT" 100, "WONK" 1'} ], 
+            'sans': ["Inter, sans-serif", {fontVariationSettings: '"wght" 300, "slnt" 0'} ],
+            'body': ["Inter, sans-serif", {fontVariationSettings: '"wght" 300, "slnt" 0'} ],                 
+          },
+
+          backgroundImage: {
+            'pattern-1': "url('/assets/images/watercolour1.png')",
+            'pattern-2': "url('/assets/images/watercolour3.png')",
+            'noise': "url('/assets/images/noise.jpg')",
+          },
+
+          keyframes: {
+            'kf-scroll-right' : {
+              '0%, 100%': { transform: 'translateX(-50%)' },
+              '50%': { transform: 'translateX(0%)' },
+            },
+            'kf-pulse': {
+              '0%, 100%': { opacity: 0 },
+              '50%': { opacity: 1 },
+            },
+            'kf-fade': {
+              '0%, 100%': {opacity: 1},
+              '50%': {opacity: 0},
+            },  
+          },
+
+          animation: {
+            'scroll-right': '240s ease-in-out infinite kf-scroll-right',
+            'bg-1': '30s 0s infinite kf-fade',
+            'bg-2': '30s 15s infinite kf-fade',
+            'pulse': '10s infinite ease-in-out ',
+          },
+
         },
       },
     },
--- scripts/word-scroll.js
+++ scripts/word-scroll.js
@@ -5,42 +5,36 @@ const scrollContainer = document.getElementById('scroll-container');
 let intervalId = null;
 
 function generateWordList() {
-    scrollContainer.innerHTML = ''; // Remove existing words
-
-    currentIndex = (currentIndex + 1) % words.length; // Cycle through words
-
-    for (let i = 0; i < 4; i++) {
-        const wordIndex = (currentIndex + i) % words.length;
-        const wordDiv = document.createElement('div');
-        wordDiv.classList.add('word');
-        if (i === 1) wordDiv.classList.add('active'); // The second word is "active"
-        wordDiv.textContent = words[wordIndex];
-        scrollContainer.appendChild(wordDiv);
-    }
+  scrollContainer.innerHTML = ''; // Remove existing words  
+  currentIndex = (currentIndex + 1) % words.length; // Cycle through words  
+  for (let i = 0; i < 4; i++) {
+    const wordIndex = (currentIndex + i) % words.length;
+    const wordDiv = document.createElement('div');
+    wordDiv.style.opacity = (i === 1) ? 1 : 0.1; // The second word is "active"
+    wordDiv.className = 'flex flex-col items-start justify-center transition-opacity duration-[2s] h-[4.5rem]';
+    wordDiv.textContent = words[wordIndex];
+    scrollContainer.appendChild(wordDiv);
+  }
 }
 
 function animate() {
-    // Reset position
-    scrollContainer.style.transform = 'translateY(0%)'; 
-
-    // Change opacity of first and second words
-    scrollContainer.children[1].classList.remove('active');
-    scrollContainer.children[2].classList.add('active');
-
-    setTimeout(() => {
-        scrollContainer.style.transition = 'transform 1s cubic-bezier(.6,-0.3,.4,1.3)'; // Add transition
-        scrollContainer.style.transform = 'translateY(25%)'; // Animate downwards
-
-        setTimeout(() => { // Delay reset to ensure animation ends
-            scrollContainer.style.transition = 'none'; // Remove transition
-            scrollContainer.style.transform = '';
-            generateWordList(); // Generate new word list
-
-            // Clear the interval and set it again
-            clearInterval(intervalId);
-            intervalId = setInterval(animate, 2500);
-        }, 2000);
-    }, 0);
+  // Reset position
+  scrollContainer.style.transform = 'translateY(0%)'; 
+  // Change opacity of first and second words
+  scrollContainer.children[1].style.opacity = 0.1;
+  scrollContainer.children[2].style.opacity = 1;
+  setTimeout(() => {
+    scrollContainer.style.transition = 'transform 1s cubic-bezier(.6,-0.3,.4,1.3)'; // Add transition
+    scrollContainer.style.transform = 'translateY(25%)'; // Animate downwards
+    setTimeout(() => { // Delay reset to ensure animation ends
+      scrollContainer.style.transition = 'none'; // Remove transition
+      scrollContainer.style.transform = '';
+      generateWordList(); // Generate new word list
+      // Clear the interval and set it again
+      clearInterval(intervalId);
+      intervalId = setInterval(animate, 2500);
+    }, 2000);
+  }, 0);
 }
 
 // Generate the initial word list
