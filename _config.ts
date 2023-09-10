4a1fedac6c482ce18ca0cb832fbd47b9bae40f18
Robert O’Brien <82072426+wolstaeb@users.noreply.github.com>
2023-09-10 20:26:49 +1200

    CHORE: Switching SASS, removing tailwind

--- _config.ts
+++ _config.ts
@@ -1,7 +1,6 @@
 import lume from "lume/mod.ts";
+import sass from "lume/plugins/sass.ts";
 import date from "lume/plugins/date.ts";
-import tailwindcss from "lume/plugins/tailwindcss.ts";
-import postcss from "lume/plugins/postcss.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
@@ -21,118 +20,6 @@ site
   .ignore("README.md", "flake.nix", "flake.lock", "default.nix", "shell.nix")
   .copy("assets")
   .copy("scripts")
-  .use(jsx())
-  .use(tailwindcss({
-    // Extract the classes from HTML and njk files
-    extensions: [".html", ".njk", ".tsx", ".js", ".css"],
-
-    // Tailwind options, like the theme colors and fonts
-    options: {
-
-      plugins: [
-        function ({ addUtilities }) {
-          addUtilities({
-            '.mask-radial': {
-              maskImage: 'radial-gradient(67% 50% at 33% 50%, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 80%)',
-              WebkitMaskImage: 'radial-gradient(67% 50% at 33% 50%, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 80%)'
-            }
-          }, ['responsive', 'hover']);
-        },
-      ],
-
-      theme: {
-        extend: {
-          colors: {
-            'coconut-cream': {
-              'DEFAULT': '#fcfaed',
-              '50': '#fcfaed',
-              '100': '#f6f1cf',
-              '200': '#ede29a',
-              '300': '#e3ce66',
-              '400': '#dcbb43',
-              '500': '#d4a02c',
-              '600': '#bb7f24',
-              '700': '#9c5e21',
-              '800': '#7f4b21',
-              '900': '#693e1e',
-              '950': '#3c200c',
-            },          
-            'english-holly': {
-              '50': '#effef2',
-              '100': '#d9ffe0',
-              '200': '#b5fdc3',
-              '300': '#7cf997',
-              '400': '#3cec62',
-              '500': '#12d53d',
-              '600': '#09b02d',
-              '700': '#0b8a27',
-              '800': '#0e6d24',
-              '900': '#0e5921',
-              'DEFAULT': '#012e0d',
-              '950': '#012e0d',
-            },             
-            'tuscany': {
-              '50': '#fcf7f0',
-              '100': '#f9ecdb',
-              '200': '#f1d7b7',
-              '300': '#e8bc89',
-              '400': '#de9759',
-              '500': '#d67c39',
-              'DEFAULT': '#cb672f',
-              '600': '#cb672f',
-              '700': '#a64f28',
-              '800': '#854027',
-              '900': '#6c3622',
-              '950': '#3a1a10',
-            },     
-            
-            // TODO: These colors are related to the mask-radial class above.
-            'black-alpha': 'rgba(0,0,0,1)',
-            'transparent': 'rgba(0,0,0,0)',
-          },
-          maxHeight: {
-            'viewport-200': 'calc(100vh - 200px)' // the class .max-h-viewport-200
-          },
-          fontFamily: {      
-            'serif': ["Fraunces, Merriweather, serif", {fontVariationSettings: '"wght" 400, "SOFT" 100, "WONK" 1'} ],  
-            'header': ["Fraunces, Merriweather, serif", {fontVariationSettings: '"wght" 400, "SOFT" 100, "WONK" 1'} ], 
-            'fine-print': ["Fraunces, Merriweather, serif", {fontVariationSettings: '"wght" 200, "SOFT" 100, "WONK" 1'} ], 
-            'sans': ['var(--font-family)', {fontVariationSettings: '"wght" 300, "slnt" 0'} ],
-            'body': ['var(--font-family)', {fontVariationSettings: '"wght" 300, "slnt" 0'} ],                 
-          },
-
-          backgroundImage: {
-            'pattern-1': "url('/assets/images/watercolour1.webp')",
-            'pattern-2': "url('/assets/images/watercolour3.webp')",
-            'noise': "url('/assets/images/noise.webp')",
-          },
-
-          keyframes: {
-            'kf-scroll-right' : {
-              '0%, 100%': { transform: 'translate3d(0, -50%, 0)' },
-              '50%': { transform: 'translate3d(0, 0, 0)' },
-            },
-            'kf-pulse': {
-              '0%, 100%': { opacity: 0 },
-              '50%': { opacity: 1 },
-            },
-            'kf-fade': {
-              '0%, 100%': {opacity: 1},
-              '50%': {opacity: 0},
-            },  
-          },
-
-          animation: {
-            'scroll-right': '240s ease-in-out infinite kf-scroll-right',
-            'bg-1': '30s 0s infinite kf-fade',
-            'bg-2': '30s 15s infinite kf-fade',
-            'pulse': '10s infinite ease-in-out',
-          },
-        },
-      },
-    },
-  }))
-  .use(postcss())
   .use(date())
   .use(codeHighlight())
   .use(basePath())
@@ -157,7 +44,8 @@ site
     }
   }))
   .use(resolveUrls())
+  .use(imagick())
   .use(picture())
-  .use(imagick());
+  .use(sass());
 
 export default site;
